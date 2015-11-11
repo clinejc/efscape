c3222409221a30a5f30a20797e822d3d88991d19
Jon C. Cline <jon.c.cline@gmail.com>
2015-11-11 10:49:22 -0500

    Added boost c++ demangle definitions (for future use)

--- src/efscape/utils/type.hpp
+++ src/efscape/utils/type.hpp
@@ -4,12 +4,14 @@
 // Copyright (C) 2006-2014 by Jon C. Cline (clinej@alumni.stanford.edu)
 // Distributed under the terms of the LGPLv3 or newer.
 // __COPYRIGHT_END__
+// 2015-11-08 from http://www.gilgil.net/communities/9180 (How to demangel a class name with Boost)
 // 2014-08-31 from http://stackoverflow.com/questions/281818/unmangling-the-result-of-stdtype-infoname
 
 #ifndef TYPE_HPP
 #define TYPE_HPP
 
 #include <string>
+#include <boost/units/detail/utility.hpp>
 #include <typeinfo>
 
 namespace efscape {
@@ -18,8 +20,8 @@ namespace efscape {
 
     template <class T>
     std::string type(const T& t) {
-
-      return demangle(typeid(t).name());
+      // return demangle(typeid(t).name());
+      return boost::units::detail::demangle(typeid(t).name());
     }
 
   }
